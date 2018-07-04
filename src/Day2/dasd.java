db1044c2f3d66d9db92b0c49d1b2f278b48163e2
Unknown <wlxm9999@naver.com>
2018-07-04 21:25:16 +0900

    for문 예

---
+++ src/Day2/dasd.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class dasd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		int i=0;
+		for(i=0;i<=10;i++){
+			sum = sum + i;
+	     System.out.println("1부터 "+i+"까지의 합 : "+sum);
+		}System.out.println("한번만나옴 : "+sum);
+	}
+
+}
