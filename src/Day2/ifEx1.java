385ff6092cc8000c21322ae143c0c1762c98dc1e
Unknown <wlxm9999@naver.com>
2018-07-04 17:01:28 +0900

    2회차 : if문 복습 예제

---
+++ src/Day2/ifEx1.java
@@ -0,0 +1,19 @@
+package Day2;
+
+public class ifEx1 {
+
+	public static void main(String[] args) {
+		/* 정수가 홀수인지 짝수인지 판별하는 코드를 if문을 이용하여 작성
+		 * */
+		int num = 7;
+		if(num%2==0){
+			System.out.println(num+"(은)는 짝수");
+			
+		}
+		else{
+			System.out.println(num+"(은)는 홀수");
+		}
+
+	}
+
+}
